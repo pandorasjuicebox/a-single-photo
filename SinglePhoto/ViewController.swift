d9d9c8762687ad32da8269263cb3eb7537bae4f8
Charina Duenas <charinaduenas@gmail.com>
2019-01-13 02:26:57 -0600

    Initial Commit

---
+++ SinglePhoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SinglePhoto
+//
+//  Created by Lockhart on 2019-01-12.
+//  Copyright © 2019 Lockhart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
